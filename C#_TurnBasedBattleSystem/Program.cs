d87cde617578ad2680bdea777dae80c53ae13ed8
HaleyOtaku <haleyotaku@gmail.com>
2024-07-03 12:18:10 -0500

    Final commit! Added break to game in the event player or enemy wins the battle.

--- C#_TurnBasedBattleSystem/Program.cs
+++ C#_TurnBasedBattleSystem/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace C__TurnBasedBattleSystem
+﻿using C__TurnBasedBattleSystem;
+
+Unit player = new Unit(100,20,12,"Player");
+Unit enemy = new Unit(75, 10, 7, "Enemy Mage");
+Random random = new Random();
+
+while (!player.IsDead && !enemy.IsDead)
 {
-    internal class Program
+    Console.WriteLine($"{player.UnitName} HP: {player.Hp} | {enemy.UnitName} HP: {enemy.Hp}\n");
+
+
+    Console.Write("Player turn! What will you do? ");
+    string choice = Console.ReadLine();
+    Console.Clear();
+
+    if (choice == "a")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        player.Attack(enemy);
+    }
+    else
+    {
+        player.Heal();
+    }
+
+    Console.WriteLine($"{player.UnitName} HP: {player.Hp} | {enemy.UnitName} HP: {enemy.Hp}\n");
+
+    Console.WriteLine("Enemy turn!");
+
+    //This generates a random number between 0-1 (The second number is exclusive, meaning not included in the range)
+    int rand = random.Next(0,2);
+
+    if  (rand == 0)
+    {
+        enemy.Attack(player);
+    }
+    else
+    {
+        enemy.Heal();
+    }
+
+    if (player.IsDead || enemy.IsDead) 
+    {
+        break;
     }
 }
---
+++ C#_TurnBasedBattleSystem/Unit.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__TurnBasedBattleSystem
+{
+    internal class Unit
+    {
+        //FIELDS
+        //In C#, a field stores a piece of data within an object
+        //If no access modifier is provided, a field is private by default.
+        //declaring objects(fields) to use later
+        int currentHp;
+        int maxHp;
+        int attackPower;
+        int healPower;
+        string unitName;
+
+        //For attack and crit chance
+        private Random random;
+
+
+        //PROPERTIES
+
+        //To display health before and after attacking
+        public int Hp { get { return currentHp; }}
+        public string UnitName { get { return unitName; } }
+
+        //For Death functionality
+        public bool IsDead { get { return currentHp <= 0; } }
+
+        //COLLECT/CATCH/CONSTRUCTORS
+        public Unit(int maxHp, int attackPower, int healPower, string unitName)
+        {
+            //instantiate the fields (make new instances of each field)
+            this.maxHp = maxHp;
+            this.currentHp = maxHp;
+            this.attackPower = attackPower;
+            this.healPower = healPower; 
+            this.unitName = unitName;
+            this.random = new Random();
+        }
+
+
+        //METHODS (Doing something with the now-existing fields)
+        public void Attack(Unit unitToAttack)
+        {
+            //Will return us a double value between 0-1
+            //When attacking our unit should be able to deal damage between 75% of our original attack power and up to 125%.
+            //Any higher than 100% will be considered a critical hit.
+            //rng = Random Number Generator
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            //               casting attackPower * rng to an int
+            int randDamage = (int)(attackPower * rng);
+            unitToAttack.TakeDamage(randDamage);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{unitName} attacks {unitToAttack.unitName} for {randDamage} damage!\n");
+            Console.ResetColor();
+        }
+
+        public void TakeDamage(int damage) 
+        { 
+            currentHp -= damage;
+            if (IsDead)
+            {
+                Console.WriteLine($"{UnitName} has been defeated!");
+            }
+        }
+
+        public void Heal()
+        {
+            //generates new random number
+            double rng = random.NextDouble();
+
+
+            rng = rng / 2 + 0.75;
+            int heal = (int)(rng * attackPower);
+
+            //Is the heal added to the currentHp higher than the maxHp? If so, set currentHp to maxHp. If not, add heal to currentHp.
+            currentHp = heal + currentHp > maxHp ? maxHp : currentHp + heal;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{UnitName} heals for {heal}!");
+            Console.ResetColor();
+        }
+
+    }
+    
+    
+}
